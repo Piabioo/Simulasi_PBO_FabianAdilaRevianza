dec3ccf240da8a9ff4b21cb4a24c94bf79775c96
Piabioo <f4721430@gmail.com>
2026-06-19 10:27:40 +0700

    tahap 3 menambahkan file koneksi dan abstract

---
+++ koneksi.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $db_name = "nama_database_anda"; // Sesuaikan dengan nama database Anda
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch(PDOException $exception) {
+            echo "Koneksi database gagal: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
